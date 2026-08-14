54b614795668f4a911653fc5dd7158c92b47b457
José Gabriel Leite Carvalho Da Silva <josegabrieleite753@gmail.com>
2026-08-14 19:45:41 -0300

    Limpeza e ajuste do gitignore

--- beauty-coast-backend/src/main/java/com/beautycoast/repository/AppointmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.beautycoast.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.beautycoast.model.Appointment;
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-}
